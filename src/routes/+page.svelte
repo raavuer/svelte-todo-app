2d0d658bb884326ee75f111379f40d92ec0eab4f
Micah <micahronan@proton.me>
2026-05-18 13:45:06 -0500

    Added basic todo input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let todos: string[] = $state([]);
+</script>
+
+<main>
+	<h1>Todo List</h1>
+
+	<input
+		type="text"
+		onchange={function () {
+			todos.push(this.value);
+		}}
+	/>
+
+	<ul>
+		{#each todos as todo (todos.indexOf(todo))}
+			<li>{todo}</li>
+		{/each}
+	</ul>
+</main>
+
+<style>
+	main {
+		display: grid;
+		place-items: center;
+		font-family: system-ui, 'Segoe UI', Oxygen, 'Open Sans', sans-serif;
+	}
+</style>
