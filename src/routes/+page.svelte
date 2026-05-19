4f9b1c52f15ecfe89d663cc1aebe2cceb5459345
Micah <micahronan@proton.me>
2026-05-18 21:50:01 -0500

    Fixed removeTodo(), and Added slide-in animation for todos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,21 @@
-<script lang="ts">
+<script>
 	import { onMount } from "svelte";
 
-	let todos: string[] = $state([]);
+	let todos = $state([]);
 	onMount(() => {
 		todos = JSON.parse(localStorage.getItem("todos") ?? "[]");
 	});
 
 	function addTodo() {
 		const todo = this.value;
-		if (todo === "") return;
+		if (todo === "" || todos.includes(todo)) return;
 		todos.push(todo);
 		localStorage.setItem("todos", JSON.stringify(todos));
 		this.value = "";
 	}
 	function removeTodo() {
-		todos.splice(this.index, 1);
+		const index = this.parentElement.id.slice(0, 1);
+		todos.splice(index, 1);
 		localStorage.setItem("todos", JSON.stringify(todos));
 	}
 </script>
@@ -25,8 +26,8 @@
 	<input type="text" onchange={addTodo} />
 
 	<ul>
-		{#each todos as todo, index (index)}
-			<li>
+		{#each todos as todo, index (todo)}
+			<li id={index + "-" + todo}>
 				<button type="button" onclick={removeTodo}>X</button>
 				<span>{todo}</span>
 			</li>
@@ -38,10 +39,43 @@
 	main {
 		display: grid;
 		place-items: center;
-		font-family: system-ui, "Segoe UI", Oxygen, "Open Sans", sans-serif;
+
+		font:
+			2rem system-ui,
+			"Segoe UI",
+			Oxygen,
+			"Open Sans",
+			sans-serif;
+	}
+	h1 {
+		font-size: 2em;
+	}
+	input {
+		font-size: 1em;
+		padding: 1rem 2rem;
+		border-radius: 16px;
 	}
 	li {
-		padding: 0.25rem;
+		display: grid;
+		grid-template-columns: 2rem 1fr;
+		gap: 8px;
+		padding: 8px;
 		list-style-type: none;
+
+		animation-name: slide-in;
+		animation-duration: 500ms;
+	}
+	button {
+		border-radius: 16px;
+	}
+	@keyframes slide-in {
+		from {
+			translate: 100% 0;
+			opacity: 0;
+		}
+		to {
+			translate: 0 0;
+			opacity: 1;
+		}
 	}
 </style>
